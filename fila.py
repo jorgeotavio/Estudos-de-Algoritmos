fac0b6e6d435da6461fb839a15fd27af3df17be6
jorge <jorgelosb@gmail.com>
2019-01-26 18:46:18 -0300

    Algoritmo segunda VA

--- fila.py
+++ fila.py
@@ -1,4 +1,4 @@
-class Pilha:
+class Fila:
     def __init__(self):
         self.primeiro = None
         self.ultimo = None
@@ -7,78 +7,86 @@ class Pilha:
 
     class No:
         def __init__(self, conteudo):
-            self.proximo = None
-            self.anterior = None
             self.conteudo = conteudo
+            self.proximo = None
 
     def __len__(self):
         return self.__tamanho
 
-    def __repr__(self):
-        return self.__str__()
+    def __getitem__(self, item):
+        if self.primeiro is not None:
+            atual = self.primeiro
+            indice = 0
+            while atual is not None:
+                if item == indice:
+                    return atual.conteudo
+
+                atual = atual.proximo
+                indice += 1
 
     def __iter__(self):
         return self
-
+    
     def __next__(self):
         if self.__iterando is None:
             self.__iterando = self.primeiro
         else:
             self.__iterando = self.__iterando.proximo
-
+        
         if self.__iterando is not None:
             return self.__iterando.conteudo
-
+        
         raise StopIteration
 
-    def __getitem__(self, i):
-        if self.ultimo is not None:
-            atual = self.ultimo
-            indice = len(self)-1
-            while indice >= 0:
-                if i == indice:
-                    return atual.conteudo
-
-                atual = atual.anterior
-                indice -= 1
+    def __repr__(self):
+        return self.__str__()
 
     def __str__(self):
         retorno = '['
+
         for i, e in enumerate(self):
-            retorno += e.__repr__()
+            retorno += repr(e)
             if i < len(self)-1:
-                retorno += ', '
+                retorno += ','
 
         retorno += ']'
         return retorno
-
-
+    
     def __delitem__(self, i):
-        if self.__tamanho != 0:
-            atual = self.ultimo
-            self.ultimo = self.ultimo.anterior
-            self.ultimo.proximo = None
-            atual.anterior = None
+        if self.primeiro is not None:
+            atual = self.primeiro
+            self.primeiro = self.primeiro.proximo
             atual.proximo = None
-
+            
             self.__tamanho -= 1
             self.__iterando = None
-
+    
     def inserir(self, conteudo):
         novo = self.No(conteudo)
-
+        
         if self.__tamanho == 0:
             self.primeiro = novo
             self.ultimo = novo
-
+        
         else:
-            last = self.ultimo
             self.ultimo.proximo = novo
             self.ultimo = novo
-            novo.anterior = last
-
+            
         self.__tamanho += 1
         self.__iterando = None
-
+    
     def pop(self):
-        del self[len(self)-1]
+        del self[0]
+
+fila = Fila()
+
+fila.inserir(23)
+fila.inserir(4)
+fila.inserir(5)
+fila.inserir(6)
+fila.inserir(19)
+
+fila.pop()
+
+print(fila)
+
